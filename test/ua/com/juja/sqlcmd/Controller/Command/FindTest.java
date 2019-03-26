3a057ba8f62a325ec6c067b41da95c387df25b78
Valentin <vradchenko@ioc.gov.ua>
2019-03-26 16:36:52 +0200

    Refactoring test

--- test/ua/com/juja/sqlcmd/Controller/Command/FindTest.java
+++ test/ua/com/juja/sqlcmd/Controller/Command/FindTest.java
@@ -5,8 +5,11 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
+
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 import ua.com.juja.sqlcmd.controller.command.Command;
+import ua.com.juja.sqlcmd.controller.command.Exit;
 import ua.com.juja.sqlcmd.controller.command.Find;
 import ua.com.juja.sqlcmd.model.DataSet;
 import ua.com.juja.sqlcmd.model.DatabaseManager;
@@ -14,29 +17,24 @@ import ua.com.juja.sqlcmd.view.View;
 
 import javax.lang.model.util.Types;
 
-import static org.junit.Assert.assertEquals;
-
 public class FindTest {
 
 
     private DatabaseManager manager;
     private View view;
+    private Find command;
 
     @Before
     public void setup(){
 
     manager= mock(DatabaseManager.class);
     view=mock( View.class );
+    command= new Find( manager, view );
     }
 
 
     @Test
     public void Test(){
-
-    Command command= new Find( manager, view );
-
-
-
 
         when( manager.getTableColumns( "user" ) ).thenReturn( new String[]{"id","name","password"} );
 
@@ -62,6 +60,46 @@ public class FindTest {
   assertEquals("[--------------------, |id|name|password|, --------------------, |45|Stiv|dddddd|, |433|Stiverr|dd3333d|]",captor.getAllValues().toString());
 
 
+    }
+
+@Test
+    public void testCanProcessFind(){
+
+
+        Boolean canProcess=command.canProcess( "find|user" );
+
+        assertTrue(canProcess);
+    }
+    @Test
+    public void testCantProcessFindString(){
+
+
+        Boolean canProcess=command.canProcess( "find" );
+
+        assertFalse(canProcess);
+    }
+
+    @Test
+    public void testProcessFindCommand_QWE(){
+
+
+
+        Boolean canProcess=command.canProcess( "qwe|user" );
+
+        assertFalse(canProcess);
+
+    }
+    @Test
+    public void TestEmpty(){
+
+        when( manager.getTableColumns( "user" ) ).thenReturn( new String[]{"id","name","password"} );
+        DataSet [] data=new DataSet[0];
+        when( manager.getTableData( "user" ) ).thenReturn( data );
+        command.process( "find|user" );
+
+        ArgumentCaptor<String> captor=ArgumentCaptor.forClass( String.class );
+        verify( view,atLeastOnce() ).write(captor.capture() );
+        assertEquals("[--------------------, |id|name|password|, --------------------]",captor.getAllValues().toString());
     }
 
 }
